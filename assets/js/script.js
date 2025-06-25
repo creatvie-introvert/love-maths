9820cb1ee69f33588a4096baf0ad6852f8512d90
urbanIntrovert <trefoil72@gmail.com>
2025-06-25 16:00:05 +0100

    feat: add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,9 @@ function runGame(gameType) {
     if (gameType === 'addition') {
         displayAdditionQuestion(num1, num2);
     }
+    else if (gameType === 'multiply') {
+        displayMultiplyQuestion(num1, num2);
+    }
     else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. aborting`;
@@ -70,6 +73,9 @@ function calculateCorrectAnswer() {
     if (operator === '+') {
         return [operand1 + operand2, 'addition'];
     }
+    else if (operator === 'x') {
+        return [operand1 * operand2, 'multiply'];
+    }
     else {
         alert(`unimplented operator ${operator}`);
         throw `unimplented operator ${operator}. aborting`;
@@ -102,6 +108,8 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = 'x';
 }
