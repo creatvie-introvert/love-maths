abb816952f0bdff50701f021d0f07e4ae8ae7888
urbanIntrovert <trefoil72@gmail.com>
2025-06-25 16:13:28 +0100

    feat: add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,9 @@ function runGame(gameType) {
     else if (gameType === 'multiply') {
         displayMultiplyQuestion(num1, num2);
     }
+    else if (gameType === 'subtract') {
+        displaySubtractQuestion(num1, num2);
+    }
     else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. aborting`;
@@ -76,6 +79,9 @@ function calculateCorrectAnswer() {
     else if (operator === 'x') {
         return [operand1 * operand2, 'multiply'];
     }
+    else if (operator === '-') {
+        return [operand1 - operand2, 'subtract'];
+    }
     else {
         alert(`unimplented operator ${operator}`);
         throw `unimplented operator ${operator}. aborting`;
@@ -104,8 +110,10 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = '+';
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '-';
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
